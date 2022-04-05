3903c8d432b113d8092bd41baaedd57762c9a8cc
Chainlayer CI <hello@chainlayer.io>
2022-04-05 13:19:39 +0000

    Switch to RPC URL instead of Q public indexer

--- README.md
+++ README.md
@@ -20,15 +20,47 @@ systemctl enable nodeexporter.service
 systemctl start nodeexporter.service
 ```
 
-* Copy the .env.example file to .env and edit your validator address
+* Copy the .env.example file to .env and edit your validator address and rpc_url
 * Add the script to cron, for example:
 
 ```
 * * * * * /root/q-monitoring/getminedblocks.sh >> /root/q-monitoring/getminedblocks.log 2>>/root/q-monitoring/getminedblocks.err
 ```
 
+Finally make sure to enable clique on your Q fullnode with --http.api=...,clique
+Edit Services > Node > Entrypoint.
+```
+entrypoint: ["geth", "--metrics", "--metrics.addr", "0.0.0.0", "--metrics.port", "5054", "--datadir=/data", ...snip... , "--http.corsdomain=*", "--http.api=net,web3,eth,debug,clique" ]
+```
+
 ### Prometheus Alert Rule
+
+The script will give you the following metrics:
+```
+# How many blocks the validator mined over the past 101 blocks
+q_mined_blocks 8
+# How many blocks the validator should have reasonably mined over the past 101 blocks
+q_expected_blocks 7
+# How many more or less blocks the validator mined compared to the expectations
+q_delta_blocks 1
+# How many blocks were mined in-turn
+q_mined_inturn 44
+# How many blocks were mined out-of-turn
+q_mined_outturn 57
+# Number of active validators in the last block
+q_active_validators 14
+```
+
+For alerting on alertmanager use these expressions
+```
+# Not proposing blocks at all
 expr: q_mined_blocks < 1
+
+or
+# Proposing less blocks than you'd expect to propose
+expr: q_delta_blocks < 0
+```
+
 
 ### Geth Metrics
 
@@ -52,3 +84,4 @@ ports:
 - 5054:5054/tcp
 - 5054:5054/udp
 ```
+
--- getminedblocks.sh
+++ getminedblocks.sh
@@ -5,13 +5,48 @@ cd $DIR
 # Source .env file
 . .env
 
-COUNT=`curl -s "https://indexer.q.org/blocks?sort=-block&page%5Blimit%5D=101"|jq '[.data[]|select(.attributes.miner=="'$VALIDATOR'")]|length'`
+# Get current blocknumber and amount of signers
+TMP=`curl -s -H "Content-type: Application/Json" -X POST --data '{"jsonrpc":"2.0","method":"clique_getSnapshot","params":["latest"],"id":1}' $RPC_URL|jq -r '"\(.result.number) \(.result.signers|length)"'`
+LASTDEC=`echo $TMP|awk -F\  '{print $1}'`
+NUMVALIDATORS=`echo $TMP|awk -F\  '{print $2}'`
+START=$(($LASTDEC - 100))
 
-re='^[0-9]+$'
-if ! [[ "$COUNT" =~ $re ]] ; then
-  echo q_mined_blocks 0 >/var/lib/node_exporter/textfile_collector/q-monitoring.prom.tmp
-else
-  echo q_mined_blocks $COUNT >/var/lib/node_exporter/textfile_collector/q-monitoring.prom.tmp
-fi
-echo $COUNT
+INTURN=0
+OUTTURN=0
+SIGNED=0
+
+for (( c=$START; c<=$LASTDEC; c++ ))
+do
+	BLOCKHEX=0x`echo "obase=16; $c" | bc`
+	SIGNER=`curl -s -H "Content-type: Application/Json" -X POST --data '{"jsonrpc":"2.0","method":"clique_getSnapshot","params":["'$BLOCKHEX'"],"id":1}' $RPC_URL|jq -r '.result.recents."'$c'"'`
+	DIFF=`curl -s -H "Content-type: Application/Json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["'$BLOCKHEX'", false],"id":1}' $RPC_URL|jq -r '"\(.result.difficulty)"'`
+	if [ $DIFF == "0x2" ] 
+	then
+		INTURN=$(($INTURN+1))
+	fi
+	if [ $DIFF == "0x1" ] 
+	then
+		OUTTURN=$(($OUTTURN+1))
+	fi
+	if [ "$SIGNER" == "$VALIDATOR" ]
+	then
+		SIGNED=$(($SIGNED+1))
+	fi
+done
+
+EXPECTED=`echo "101 / $NUMVALIDATORS"|bc`
+DELTA=$(($SIGNED - $EXPECTED))
+>/var/lib/node_exporter/textfile_collector/q-monitoring.prom.tmp
+echo "# How many blocks the validator mined over the past 101 blocks" >>/var/lib/node_exporter/textfile_collector/q-monitoring.prom.tmp
+echo q_mined_blocks $SIGNED >>/var/lib/node_exporter/textfile_collector/q-monitoring.prom.tmp
+echo "# How many blocks the validator should have reasonably mined over the past 101 blocks" >>/var/lib/node_exporter/textfile_collector/q-monitoring.prom.tmp
+echo q_expected_blocks $EXPECTED >>/var/lib/node_exporter/textfile_collector/q-monitoring.prom.tmp
+echo "# How many more or less blocks the validator mined compared to the expectations" >>/var/lib/node_exporter/textfile_collector/q-monitoring.prom.tmp
+echo q_delta_blocks $DELTA >>/var/lib/node_exporter/textfile_collector/q-monitoring.prom.tmp
+echo "# How many blocks were mined in-turn" >>/var/lib/node_exporter/textfile_collector/q-monitoring.prom.tmp
+echo q_mined_inturn $INTURN >>/var/lib/node_exporter/textfile_collector/q-monitoring.prom.tmp
+echo "# How many blocks were mined out-of-turn" >>/var/lib/node_exporter/textfile_collector/q-monitoring.prom.tmp
+echo q_mined_outturn $OUTTURN >>/var/lib/node_exporter/textfile_collector/q-monitoring.prom.tmp
+echo "# Number of active validators in the last block" >>/var/lib/node_exporter/textfile_collector/q-monitoring.prom.tmp
+echo q_active_validators $NUMVALIDATORS >>/var/lib/node_exporter/textfile_collector/q-monitoring.prom.tmp
 mv /var/lib/node_exporter/textfile_collector/q-monitoring.prom.tmp /var/lib/node_exporter/textfile_collector/q-monitoring.prom
